17430d1529606cc6e9caeb3703658768f14e7b38
Stephen O'Dwyer <67125983+angrieralien@users.noreply.github.com>
2023-04-12 15:36:36 -0400

    fixes OpenFile lock

--- ingesters/namedpipe/namedpipeingester.go
+++ ingesters/namedpipe/namedpipeingester.go
@@ -13,11 +13,25 @@ type NamedPipeIngester struct {
 }
 
 func (a *NamedPipeIngester) Ingest(ctx context.Context, tailProcessor TailProcessor, logger *zap.SugaredLogger, h *common.Health) error {
-	file, err := os.OpenFile(a.FilePath, os.O_RDONLY, os.ModeNamedPipe)
+	var file *os.File
+	var err error
+	ready := make(chan struct{})
+	// os.OpenFile blocks. Put in go routine so we can gracefully exit.
+	go (func() {
+		file, err = os.OpenFile(a.FilePath, os.O_RDONLY, os.ModeNamedPipe)
+		close(ready)
+	})()
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case <-ready:
+	}
+
 	if err != nil {
-		logger.Fatal(err)
 		return err
 	}
+
 	h.OnReady()
 	return Tail(ctx, file, logger, tailProcessor)
 }
