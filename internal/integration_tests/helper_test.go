e72fe5d187f3d03eaec461bc690c3a503b616da1
Stephen O'Dwyer <67125983+angrieralien@users.noreply.github.com>
2023-05-09 14:38:50 -0400

    adds back unused logger function

--- internal/integration_tests/helper_test.go
+++ internal/integration_tests/helper_test.go
@@ -23,6 +23,15 @@ import (
 
 var debug *log.Logger
 
+func zapLoggerConfig() *zap.Config {
+	config := zap.NewDevelopmentConfig()
+	config.EncoderConfig = zap.NewDevelopmentEncoderConfig()
+	config.DisableStacktrace = true
+	config.Level = zap.NewAtomicLevelAt(zap.ErrorLevel)
+
+	return &config
+}
+
 // setupSSHCertAccess generates a SSH CA and a private key for the current
 // user. It then issues an SSH certificate for the user's key pair.
 func setupSSHCertAccess(t *testing.T, ctx context.Context, homeDirPath string) (userKeyPairPath string) {
